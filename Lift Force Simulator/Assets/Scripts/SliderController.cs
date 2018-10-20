811d604dea47985b92ca60b8f5cf7b5ba787f476
Rajtar <jakubraj@gmail.com>
2018-10-20 20:26:20 +0200

    Add UI

---
+++ Lift Force Simulator/Assets/Scripts/SliderController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderController : MonoBehaviour
+{
+	public TextMeshProUGUI sliderValueText;
+	public int decimalPlaces;
+	public string suffix;
+	private Slider slider;
+	void Start()
+	{
+		slider = GetComponentInChildren<Slider>();
+	}
+
+    void Update()
+    {
+        sliderValueText.text = slider.value.ToString(BuildDecimalPattern()) + " " + suffix;
+    }
+
+	string BuildDecimalPattern()
+	{
+		return "0." + new String('0', decimalPlaces);
+	}
+}
