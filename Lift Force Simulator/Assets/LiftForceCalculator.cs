2664b1ea03811220725ccd627fdbe40b02c47991
BartekSurlejewski <bartoszsurlejewski@gmail.com>
2018-10-23 22:02:50 +0200

    Adding formula that calculates lift force

---
+++ Lift Force Simulator/Assets/LiftForceCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiftForceCalculator : MonoBehaviour
+{
+
+    public DoubleVariable AirDensity, AirfoilCoefficient, AttackAngle, Velocity, WingSurface;
+    public double LiftForce = 0;
+
+    void Update()
+    {
+        CalculateLiftForce();
+    }
+
+    public void CalculateLiftForce()
+    {
+        if (AirDensity && AirfoilCoefficient && AttackAngle && Velocity && WingSurface)
+        {
+            LiftForce = (2.0d * AirfoilCoefficient.Value * (AttackAngle.Value + 5.0d) * AirDensity.Value * WingSurface.Value * Mathf.Pow((float)Velocity.Value, 2)) / 2.0d;
+        }
+
+        Debug.Log(LiftForce);
+    }
+}
