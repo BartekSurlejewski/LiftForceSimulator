59ef9e4c65207eb5fcd9805a00ddd1971510cec6
BartekSurlejewski <bartoszsurlejewski@gmail.com>
2018-10-23 17:32:33 +0200

    Adding scriptable objects' values change

--- Lift Force Simulator/Assets/Scripts/SliderController.cs
+++ Lift Force Simulator/Assets/Scripts/SliderController.cs
@@ -1,35 +1,34 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.Remoting.Messaging;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SliderController : MonoBehaviour
 {
-	public TextMeshProUGUI sliderValueText;
+    public TextMeshProUGUI sliderValueText;
     public DoubleVariable SliderValue;
-    public double SliderValueAccesor
-    {
-        set { SliderValue.Value = value; }
-    }
-	public int decimalPlaces;
-	public string suffix;
+    public int decimalPlaces;
+    public string suffix;
 
-	private Slider slider;
-	void Start()
-	{
-		slider = GetComponentInChildren<Slider>();
-	}
+    private Slider slider;
+    void Start()
+    {
+        slider = GetComponentInChildren<Slider>();
+        UpdateSliderValue();
+    }
 
     void Update()
     {
         sliderValueText.text = slider.value.ToString(BuildDecimalPattern()) + " " + suffix;
     }
 
-	string BuildDecimalPattern()
-	{
-		return "0." + new String('0', decimalPlaces);
-	}
+    public void UpdateSliderValue()
+    {
+        SliderValue.Value = slider.value;
+    }
+
+    string BuildDecimalPattern()
+    {
+        return "0." + new String('0', decimalPlaces);
+    }
 }
