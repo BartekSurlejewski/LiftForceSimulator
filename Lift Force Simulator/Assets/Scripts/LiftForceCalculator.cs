5475dc5096b64615972d6584f2b1430e535e2772
BartekSurlejewski <bartoszsurlejewskI@gmail.com>
2018-10-29 19:59:53 +0100

    Adding display of lift force

---
+++ Lift Force Simulator/Assets/Scripts/LiftForceCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiftForceCalculator : MonoBehaviour
+{
+
+    public DoubleVariable AirDensity, AirfoilCoefficient, AttackAngle, Velocity, WingSurface, LiftForceVariable;
+    public double LiftForce = 0;
+
+    void Update()
+    {
+        CalculateLiftForce();
+    }
+
+    public void CalculateLiftForce()
+    {
+        if (AirDensity && AirfoilCoefficient && AttackAngle && Velocity && WingSurface)
+        {
+            LiftForce = (2.0d * AirfoilCoefficient.Value * (AttackAngle.Value + 5.0d) * AirDensity.Value * WingSurface.Value * Mathf.Pow((float)Velocity.Value, 2)) / 2.0d;
+        }
+		
+		LiftForceVariable.Value = LiftForce;
+		
+        Debug.Log(LiftForce);
+    }
+}
---
+++ Lift Force Simulator/Assets/Scripts/LiftForceTextController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LiftForceTextController : MonoBehaviour
+{
+
+    public TextMeshProUGUI LiftForceText;
+    public DoubleVariable LiftForce;
+	
+	void Update ()
+    {
+        LiftForceText.text = "Lift force = " + LiftForce.Value.ToString("0.00") + " N";
+	}
+}
