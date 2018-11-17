7b09c22adf0579d8925874754ff45825e659666e
Rajtar <jakubraj@gmail.com>
2018-11-17 16:24:11 +0100

    Add scrollable background

---
+++ Lift Force Simulator/Assets/Scripts/RepeatingBackground.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBackground : MonoBehaviour {
+      
+	public GameObject FlyingObject;
+
+    private float backgroundWidth;   
+	private float backgroundHeight;    
+
+    private void Awake ()
+    {
+        backgroundWidth = GetComponent<Renderer>().bounds.size.x;
+		backgroundHeight = GetComponent<Renderer>().bounds.size.y;
+    }
+
+    private void Update()
+    {
+        if (transform.position.x < -backgroundWidth)
+        {
+            MoveBackgroundRight();
+        }
+		
+		if(FlyingObject.transform.position.y > backgroundHeight)
+		{
+			// MoveBackgroundUp();
+		}
+    }
+
+    private void MoveBackgroundRight()
+    {
+        float groundOffSet = backgroundWidth * 2f;
+		transform.position = new Vector3(transform.position.x + groundOffSet, transform.position.y, transform.position.z);
+    }
+
+	private void MoveBackgroundUp()
+    {
+        float groundOffSet = backgroundHeight * 2f;
+		transform.position = new Vector3(transform.position.x, transform.position.y + groundOffSet, transform.position.z);
+    }
+}
---
+++ Lift Force Simulator/Assets/Scripts/ScrollingObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour {
+
+	public DoubleVariable FlyingObjectVelocity;
+
+    private Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+    void Update () 
+    {
+        rb.velocity = new Vector3(-5 * ((float) FlyingObjectVelocity.Value), 0, 0);
+    }
+}
